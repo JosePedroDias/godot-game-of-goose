05323d0657f9754ad95c119e373ae5ee16c4da88
José Pedro Dias <jose.dias@arkadium.com>
2024-04-25 17:41:50 +0100

    rotate the die programatically

--- Scripts/cell_positions.gd
+++ Scripts/cell_positions.gd
@@ -2,17 +2,31 @@ extends Node
 
 class_name CellPositions
 
+@export var die: MeshInstance3D
 var children_positions: Array[Node3D] = []
+
+var die_quats: Array[Quaternion] = []
 
 signal animation_finished
 
-var anim_duration = 0.4 #0.7
+var move_piece_duration = 0.4 #0.7
+var roll_die_step_duration = 0.5
+
+const RAD_90 = 0.5 * PI
+const RAD_180 = PI
 
 func _ready():
 	var children = get_children()
 	for n:Node in children:
 		if n.name != 'board':
 			children_positions.push_back(n)
+	
+	die_quats.push_back( Quaternion(Vector3(0.0, 0.0, 1.0), RAD_90).normalized() ) #0 (1) OK
+	die_quats.push_back( Quaternion(Vector3(1.0, 0.0, 0.0), -RAD_90).normalized() ) #1 (2) OK
+	die_quats.push_back( Quaternion().normalized() ) #2 (3) OK
+	die_quats.push_back( Quaternion(Vector3(1.0, 0.0, 0.0), RAD_180).normalized() ) #3 (4) OK
+	die_quats.push_back( Quaternion(Vector3(1.0, 0.0, 0.0), RAD_90).normalized() ) #4 (5) OK
+	die_quats.push_back( Quaternion(Vector3(0.0, 0.0, 1.0), -RAD_90).normalized() ) #5 (6) OK
 
 func get_nth_marker(cell_nr: int) -> Node3D:
 	var is_valid = cell_nr >= 0 && cell_nr < children_positions.size()
@@ -21,13 +35,35 @@ func get_nth_marker(cell_nr: int) -> Node3D:
 		cell_nr = 0
 	return children_positions[cell_nr]
 	
-func animate_to_position(piece: Node3D, cell_nr: int):
+func animate_to_position(piece: Node3D, cell_nr: int) -> void:
 	var node = get_nth_marker(cell_nr)
 	
 	var pos = node.position
 	var q = node.quaternion
 	
 	var tween = get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel(true)
-	tween.tween_property(piece, "position", pos, anim_duration)
-	tween.tween_property(piece, "quaternion", q, anim_duration)
+	tween.tween_property(piece, "position", pos, move_piece_duration)
+	tween.tween_property(piece, "quaternion", q, move_piece_duration)
 	tween.chain().tween_callback(func(): animation_finished.emit())
+
+func _random_quaternion() -> Quaternion:
+	var x = randf_range(-1.0, 1.0)
+	var y = randf_range(-1.0, 1.0)
+	var z = randf_range(-1.0, 1.0)
+	var w = randf_range(-1.0, 1.0)
+	return Quaternion(x, y, z, w).normalized()
+
+func _get_face_quaternion(face_no: int) -> Quaternion:
+	if face_no < 1 || face_no > 6:
+		print('wrong face number. expect 1-6')
+		return die.quaternion
+	return die_quats[face_no - 1]
+
+func set_die_face(face_no: int) -> void:
+	die.quaternion = _get_face_quaternion(face_no)
+
+func roll_die(face_no: int) -> void:
+	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(die, "quaternion", _random_quaternion(),          roll_die_step_duration)
+	tween.tween_property(die, "quaternion", _random_quaternion(),          roll_die_step_duration)
+	tween.tween_property(die, "quaternion", _get_face_quaternion(face_no), roll_die_step_duration)
--- Scripts/play_scene.gd
+++ Scripts/play_scene.gd
@@ -9,27 +9,30 @@ var current_cell_no: int = -1
 const last_cell_no: int = 63
 
 func _ready():
+	cp.set_die_face(randi_range(1, 6))
 	_create_piece()
 	_create_piece()
 
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
-			if event.keycode == KEY_ESCAPE:
-				get_tree().quit()
-			elif event.keycode == KEY_1:
-				cp.animate_to_position(pieces[0], 0)
-			elif event.keycode == KEY_2:
-				traverse_the_board()
+			if event.keycode == KEY_ESCAPE: get_tree().quit()
+			elif event.keycode == KEY_1: cp.roll_die(1)
+			elif event.keycode == KEY_2: cp.roll_die(2)
+			elif event.keycode == KEY_3: cp.roll_die(3)
+			elif event.keycode == KEY_4: cp.roll_die(4)
+			elif event.keycode == KEY_5: cp.roll_die(5)
+			elif event.keycode == KEY_6: cp.roll_die(6)
+			elif event.keycode == KEY_9: traverse_the_board()
 
-func _piece_movement_done():
+func _piece_movement_done() -> void:
 	_traverse_the_board()
 
-func traverse_the_board():
+func traverse_the_board() -> void:
 	cp.animation_finished.connect(_piece_movement_done)
 	_traverse_the_board()
 
-func _traverse_the_board():
+func _traverse_the_board() -> void:
 	if current_cell_no == last_cell_no:
 		cp.animation_finished.disconnect(_piece_movement_done)
 		print("all done")
@@ -38,7 +41,7 @@ func _traverse_the_board():
 		print("step %d" % current_cell_no)
 		cp.animate_to_position(pieces[0], current_cell_no)
 
-func _create_piece():
+func _create_piece() -> void:
 	var piece: MeshInstance3D = reference_piece.duplicate()
 	add_child(piece)
 
