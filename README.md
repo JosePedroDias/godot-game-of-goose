82b201358edbe1a04ad592da48d837d39a76738e
José Pedro Dias <jose.dias@arkadium.com>
2024-04-25 17:02:34 +0100

    programatically create colored pieces

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 wood texture
 https://www.flickr.com/photos/hirespic/36191787146/in/dateposted/
+
+
+## TODO
+
+- track pieces in each cell and change piece position when multiple pieces are in the same cell so they get visually split
+- programatically roll the die according to a desired outcome face
+- adapt nakama code from tic-tac-toe (library + login form)
+- connect godot client with the server game
+- create UI overlay with roll button and a label to provide feedback to the player
--- Scripts/play_scene.gd
+++ Scripts/play_scene.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
 @onready var cp: CellPositions = $CellPositions #get_node("CellPositions")
-@onready var piece: MeshInstance3D = $piece # $die
+@onready var reference_piece: MeshInstance3D = $piece # $die
+
+var pieces: Array[MeshInstance3D] = []
 
 var current_cell_no: int = -1
 const last_cell_no: int = 63
+
+func _ready():
+	_create_piece()
+	_create_piece()
 
 func _input(event):
 	if event is InputEventKey:
@@ -12,10 +18,8 @@ func _input(event):
 			if event.keycode == KEY_ESCAPE:
 				get_tree().quit()
 			elif event.keycode == KEY_1:
-				#cp.animation_finished.connect(_done, CONNECT_ONE_SHOT)
-				cp.animate_to_position(piece, 0)
+				cp.animate_to_position(pieces[0], 0)
 			elif event.keycode == KEY_2:
-				#cp.animate_to_position(piece, 0)
 				traverse_the_board()
 
 func _piece_movement_done():
@@ -32,4 +36,18 @@ func _traverse_the_board():
 	else:
 		current_cell_no += 1
 		print("step %d" % current_cell_no)
-		cp.animate_to_position(piece, current_cell_no)
+		cp.animate_to_position(pieces[0], current_cell_no)
+
+func _create_piece():
+	var piece: MeshInstance3D = reference_piece.duplicate()
+	add_child(piece)
+
+	piece.visible = true
+	
+	var mat = StandardMaterial3D.new()
+	mat.albedo_color = Color(randf(), randf(), randf(), 1.0)
+	piece.material_override = mat
+	
+	cp.animate_to_position(piece, 0)#pieces.size())
+	
+	pieces.push_back(piece)
