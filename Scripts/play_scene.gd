e8c66d037113ceaa7d2c302e6f8c4be539a96bea
José Pedro Dias <jose.dias@arkadium.com>
2024-05-01 01:05:29 +0100

    continuous piece movement w/ clamp to last cell

--- Scripts/play_scene.gd
+++ Scripts/play_scene.gd
@@ -6,17 +6,15 @@ extends Node3D
 var pieces: Array[MeshInstance3D] = []
 var next_to_play: Array[int] = []
 var player_cell_index: Array[int] = []
+var player_cell_destination_index: Array[int] = []
 
-#var current_cell_no: int = -1
-const last_cell_no: int = 63
+const LAST_CELL_NO: int = 63
 
 func _ready():
 	cp.set_die_face(randi_range(1, 6))
 	_create_piece()
 	_create_piece()
 	_create_piece()
-	
-	#print( _current_player() )
 
 func _input(event) -> void:
 	if event is InputEventKey:
@@ -28,12 +26,16 @@ func _input(event) -> void:
 			elif event.keycode == KEY_4: cp.roll_die(4)
 			elif event.keycode == KEY_5: cp.roll_die(5)
 			elif event.keycode == KEY_6: cp.roll_die(6)
-			elif event.keycode == KEY_7:
-				start_next_player_round()
-			#elif event.keycode == KEY_9: traverse_the_board()
+			elif event.keycode == KEY_7: start_next_player_round()
 
 func start_next_player_round() -> void:
 	var player_index = _next_player()
+	var current_cell_no = player_cell_index[player_index]
+	
+	if current_cell_no == LAST_CELL_NO:
+		print('nowhere to move to. skip')
+		return
+	
 	var die_value = randi_range(1, 6)
 	cp.rolling_animation_finished.connect(_resume_player_round_after_rolling_die)
 	cp.roll_die(die_value)
@@ -42,25 +44,21 @@ func _resume_player_round_after_rolling_die(die_value) -> void:
 	var player_index = _current_player()
 	cp.rolling_animation_finished.disconnect(_resume_player_round_after_rolling_die)
 	var current_cell_no = player_cell_index[player_index]
-	current_cell_no += die_value
-	player_cell_index[player_index] = current_cell_no
-	cp.animate_to_position(pieces[player_index], current_cell_no) # TODO animate intermediate step
+	player_cell_index[player_index] = clamp(current_cell_no + 1, 0, LAST_CELL_NO)
+	player_cell_destination_index[player_index] = clamp(current_cell_no + die_value, 0, LAST_CELL_NO)
+	cp.animate_to_position(pieces[player_index], current_cell_no + 1)
+	cp.position_animation_finished.connect(_resume_player_round_after_moving_a_cell)
 
-#func _piece_movement_done() -> void:
-	#_traverse_the_board()
-#
-#func traverse_the_board() -> void:
-	#cp.animation_finished.connect(_piece_movement_done)
-	#_traverse_the_board()
-#
-#func _traverse_the_board() -> void:
-	#if current_cell_no == last_cell_no:
-		#cp.animation_finished.disconnect(_piece_movement_done)
-		#print("all done")
-	#else:
-		#current_cell_no += 1
-		#print("step %d" % current_cell_no)
-		#cp.animate_to_position(pieces[0], current_cell_no)
+func _resume_player_round_after_moving_a_cell() -> void:
+	var player_index = _current_player()
+	var current_cell_no = player_cell_index[player_index]
+	var destination_cell_no = player_cell_destination_index[player_index]
+	
+	if destination_cell_no == current_cell_no:
+		cp.rolling_animation_finished.disconnect(_resume_player_round_after_moving_a_cell)
+	else:
+		player_cell_index[player_index] = current_cell_no + 1
+		cp.animate_to_position(pieces[player_index], current_cell_no + 1)
 
 func _create_piece() -> void:
 	var piece: MeshInstance3D = reference_piece.duplicate()
@@ -77,6 +75,7 @@ func _create_piece() -> void:
 	pieces.push_back(piece)
 	next_to_play.push_back(next_to_play.size())
 	player_cell_index.push_back(0)
+	player_cell_destination_index.push_back(0)
 
 func _next_player() -> int:
 	var result = next_to_play.pop_front()
